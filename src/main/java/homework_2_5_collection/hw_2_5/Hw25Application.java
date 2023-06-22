003e7c787ca4eaf3626764e8aa31e8f578592bcc
mikha <Mikhailov.dzenphone@gmail.com>
2023-06-22 18:58:27 +0300

    Initial commit

---
+++ src/main/java/homework_2_5_collection/hw_2_5/Hw25Application.java
@@ -0,0 +1,13 @@
+package homework_2_5_collection.hw_2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw25Application.class, args);
+	}
+
+}
---
+++ src/test/java/homework_2_5_collection/hw_2_5/Hw25ApplicationTests.java
@@ -0,0 +1,13 @@
+package homework_2_5_collection.hw_2_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
